27db1618941ad5d1e1edbda10e62919d84e90e80
Sachini Anjana <97577090+NSachini@users.noreply.github.com>
2025-07-14 18:50:54 +0530

    Update Server.js

--- backend/server.js
+++ backend/server.js
@@ -18,6 +18,5 @@ app.use('/api/products', require('./routes/products'));
 app.use('/api/recycle', require('./routes/recycle'));
 app.use('/api/rewards', require('./routes/rewards'));
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server started on port ${port}`));
